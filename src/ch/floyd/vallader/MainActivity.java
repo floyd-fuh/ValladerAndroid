ca57170f3c59112a40e81864517cfc38dbcb642d
floyd <floyd@floyd.ch>
2015-02-10 14:16:52 +0100

    New version 1.2 with progress counter

--- src/ch/floyd/vallader/MainActivity.java
+++ src/ch/floyd/vallader/MainActivity.java
@@ -9,6 +9,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import ch.floyd.vallader.parsers.Parser;
 import ch.floyd.vallader.parsers.Searcher;
 import ch.floyd.vallader.parsers.webviews.LehrpersonenWebviewParser;
@@ -23,6 +24,8 @@ import ch.floyd.vallader.searcher.UdgSearcherGerman;
 import ch.floyd.vallader.searcher.UdgSearcherVallader;
 
 public class MainActivity extends Activity {
+	
+	ArrayList<Searcher> list = null;
 	
 	private final TextWatcher mTextEditorWatcher = new TextWatcher() {
         public void beforeTextChanged(CharSequence s, int start, int count, int after){
@@ -59,18 +62,48 @@ public class MainActivity extends Activity {
         
         EditText search_field = (EditText) findViewById(R.id.search_field);
         search_field.addTextChangedListener(mTextEditorWatcher);
+        
+        /*search_field.setOnEditorActionListener( new OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)
+                	search();
+                return false;
+            }
+        });
+        */
+        /*
+        search_field.setOnKeyListener(new OnKeyListener(){
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if (keyCode == EditorInfo.IME_ACTION_SEARCH ||
+				    keyCode == EditorInfo.IME_ACTION_DONE ||
+				    event.getAction() == KeyEvent.ACTION_DOWN &&
+				    event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+						if (!event.isShiftPressed()) {
+							Log.v("AndroidEnterKeyActivity","Enter Key Pressed!");
+							search();
+						}
+						return true;
+				 }	   
+				return false; // pass on to other listeners. 
+			}
+        });
+        */
 	}
-	
-	
-
 
 	
 	private void search(){
+		if(list != null){
+			for(Searcher s : list){
+				s.setStillCareAboutResults(false);
+			}
+		}
 		
 		final EditText search_field = (EditText) findViewById(R.id.search_field);
 		String query = search_field.getText().toString().trim();
 		if(!query.equals("")){
-			ArrayList<Searcher> list = new ArrayList<Searcher>();
+			list = new ArrayList<Searcher>();
 			list.add(new LehrpersonenSearcher(this, new Parser[]{new LehrpersonenWebviewParser(this)}));
 			
 			list.add(new PledariSearcherGerman(this, new Parser[]{
@@ -85,6 +118,8 @@ public class MainActivity extends Activity {
 			list.add(new UdgSearcherGerman(this, new Parser[]{new UdgWebviewParserGerman(this)}));
 			list.add(new UdgSearcherVallader(this, new Parser[]{new UdgWebviewParserVallader(this)}));
 			
+			((TextView) findViewById(R.id.progress)).setText(R.string.progress_start);
+			
 			for(Searcher s : list){
 				// we don't want to crash just because one parser doesn't work
 				try{
@@ -97,7 +132,19 @@ public class MainActivity extends Activity {
 		}
 	}
 	
-
+	public void updateProgress(){
+		//LayoutInflater v = a.getLayoutInflater(); 
+		//TextView progress = (TextView) v.inflate(R.id.progress, null);
+		TextView progress = (TextView) findViewById(R.id.progress);
+		if(progress != null){
+			String progressText = progress.getText().toString();
+			int i = progressText.indexOf("/");
+			int done = Integer.parseInt(progressText.substring(0, i));
+			int total = Integer.parseInt(progressText.substring(i+1));
+			done++;
+			progress.setText(done+"/"+total);
+		}
+	}
 	
 	
 	
--- src/ch/floyd/vallader/parsers/Searcher.java
+++ src/ch/floyd/vallader/parsers/Searcher.java
@@ -1,5 +1,6 @@
 package ch.floyd.vallader.parsers;
 
+import android.os.Handler;
 import ch.floyd.vallader.MainActivity;
 
 public abstract class Searcher {
@@ -10,6 +11,7 @@ public abstract class Searcher {
 	public String results_not_found_string = null;
 	public boolean results_found = true;
 	public boolean results_not_found = false;
+	public boolean stillCareAboutResults = true;
 	public String searchUrl;
 	
 	public Searcher(MainActivity a, Parser[] p){
@@ -19,6 +21,16 @@ public abstract class Searcher {
 	
 	public abstract void search(String query);
 	public void parseResult(String result){
+		if(!stillCareAboutResults)
+			return;
+		
+		(new Handler()).post(new Runnable() {
+            public void run() {
+        		a.updateProgress();
+            }
+        });
+
+
 		String result_lower = result.toLowerCase();
 		if(results_found_string != null){
 			results_found = result_lower.contains(results_found_string.toLowerCase());
@@ -30,4 +42,11 @@ public abstract class Searcher {
 			parser.parseResult(result, this);
 		}
 	}
+	
+	public void setStillCareAboutResults(boolean value){
+		stillCareAboutResults = value;
+	}
+	
+	
+	
 }
